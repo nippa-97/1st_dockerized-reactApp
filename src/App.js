4eba8d277040f1605a33e65fb0429fe09267492b
Nipun Sandeep <nipunsandeep97@gmail.com>
2022-03-14 16:30:08 +0530

    modified app.js for the teting branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Docker Testing And Git/Github testing 
+          This is a test branch for GIT 
         </p>
         <a
           className="App-link"
